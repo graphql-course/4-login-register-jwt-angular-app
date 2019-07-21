45baa4ef2e4e0d14d8a1377bfbd4499266e5eca1
Anartz Mugika Ledo <mugan86@gmail.com>
2019-07-21 17:41:03 +0200

    Publicar Angular APp

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + 'login-jwt'));
+// login-jwt
 
-app.get('/*', function (req, res) {
+app.use(express.static(__dirname + '/login-jwt'));
+
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'login-jwt', 'index.html'));
 });
 
-app.listen ( {
+app.listen({
     port: process.env.PORT || 8080
 },
-    () => console.log(`Servidor iniciado en http://localhost:8080`)   
+    ()=> console.log('Servidor iniciado http://localhost:8080')
 );
